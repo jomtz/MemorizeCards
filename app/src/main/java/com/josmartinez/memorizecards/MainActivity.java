7659d0c50181ba05e5f1e21dfa7b4fa8d46181a7
Jos Martinez <46319076+josmartinez@users.noreply.github.com>
2020-11-16 12:04:21 -0500

    Create setting menu

--- app/src/main/java/com/josmartinez/memorizecards/MainActivity.java
+++ app/src/main/java/com/josmartinez/memorizecards/MainActivity.java
@@ -1,14 +1,81 @@
 package com.josmartinez.memorizecards;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // The order message, displayed in the Toast and sent to the new Activity.
+    private String mOrderMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+
+
     }
+
+
+    /**
+     * Inflates the menu, and adds items to the action bar if it is present.
+     * @param menu Menu to inflate.
+     * @return Returns true if the menu inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles app bar item clicks.
+     * @param item Item clicked.
+     * @return True if one of the defined items was clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // This comment suppresses the Android Studio warning about simplifying
+        // the return statements.
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            mOrderMessage = getString(R.string.order_message);
+            displayToast(mOrderMessage);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Displays a Toast with the message.
+     * @param message Message to display.
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
 }
